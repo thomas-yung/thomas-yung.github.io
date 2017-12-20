a450ad7190e19040305593301e4ef8164a3698b8
Thomas Yung <thomasyung@hotmail.co.uk>
2017-12-20 19:18:53 +0000

    New blog style implemented

--- scripts/blogScript.js
+++ scripts/blogScript.js
@@ -1,16 +1,5 @@
-var articles = ["website", "tron"];
-var openArticle = null;
-
-// Return the index of the section name
-function secToNum(target) {
-  for (i = 0; i < articles.length; i++) {
-    if (target === articles[i]) {
-      return i;
-    }
-  }
-  alert("@secToNum: Defaulting to 0")
-  return 0;
-}
+var openArticle = "";
+var articleContent = "";
 
 /* ----- Menu Line Animation --- */
 function addLine(target) {
@@ -19,9 +8,7 @@ function addLine(target) {
 }
 
 function remLine(target) {
-  if (target !== openArticle) {
-    setLineWidth(target, "0%");
-  }
+  setLineWidth(target, "0%");
   return;
 }
 
@@ -31,46 +18,4 @@ function setLineWidth(target, width) {
   var underline = document.getElementById(target + '-u');
   underline.style.width = width;
   return;
-}
-
-/* ----- Section Open/Close Animation --- */
-function cover() {
-  document.getElementById("cover").style.height = "100%";
-  return;
-}
-
-function uncover() {
-  document.getElementById("cover").style.height = "0%";
-  return;
-}
-
-function hideAll() {
-  for (i = 0; i < articles.length; i++) {
-    var articleCL = document.getElementById(articles[i]+"content").classList;
-    articleCL.add("hiddenarticle");
-    articleCL.remove("activearticle");
-    remLine(articles[i]);
-  }
-  return;
-}
-
-function show(target) {
-  // Bring up cover
-  cover();
-  // Hide everything (after 1 second)
-  setTimeout(hideAll, 1000);
-  // Show intended (after 1 second)
-  setTimeout(function temp() {
-    var targetCL = document.getElementById(target+"content").classList;
-    targetCL.add("activecontent");
-    targetCL.remove("hiddenarticle");
-  }, 1000);
-  openArticle = target;
-  setTimeout(uncover, 1000);
-}
-
-
-/* ----- General Open/Close Animation --- */
-function toggleGen(section) {
-  document.getElementById(section).classList.toggle("show");
 }
