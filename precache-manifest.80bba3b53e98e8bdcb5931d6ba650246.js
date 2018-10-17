4d7cf77e0be71db101d3edd8789d077a072d064b
Thomas Yung <thomasyung@hotmail.co.uk>
2018-10-17 22:12:59 +0100

    [ty] Fix

---
+++ precache-manifest.80bba3b53e98e8bdcb5931d6ba650246.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5562e28876a57f3340c8",
+    "url": "/static/js/main.5562e288.chunk.js"
+  },
+  {
+    "revision": "7258aa498efb1b5b3320",
+    "url": "/static/js/1.7258aa49.chunk.js"
+  },
+  {
+    "revision": "5562e28876a57f3340c8",
+    "url": "/static/css/main.cde8c9cd.chunk.css"
+  },
+  {
+    "revision": "2fc649e12bbefa6b6eedd9ff8f715e28",
+    "url": "/index.html"
+  }
+];
