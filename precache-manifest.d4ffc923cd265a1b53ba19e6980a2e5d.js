c61780f748a20553f2fa0020cbadb47e8daf023f
Thomas Yung <thomasyung@hotmail.co.uk>
2018-12-16 15:39:49 +0000

    added missing files

---
+++ precache-manifest.d4ffc923cd265a1b53ba19e6980a2e5d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2e705d368c0e4def6e16",
+    "url": "/static/js/main.2e705d36.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "2e705d368c0e4def6e16",
+    "url": "/static/css/main.e10f8e94.chunk.css"
+  },
+  {
+    "revision": "86f9d27768ed9c4102cd0c1f128333f9",
+    "url": "/index.html"
+  }
+];
