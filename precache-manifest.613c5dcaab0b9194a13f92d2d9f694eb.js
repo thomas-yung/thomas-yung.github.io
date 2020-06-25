415515787fc6132feedf9d1ae839140cd3dd0dfa
Thomas Yung <yung.thomas@outlook.com>
2020-06-25 14:32:59 +0100

    Update with fourth year (add missing files)

---
+++ precache-manifest.613c5dcaab0b9194a13f92d2d9f694eb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a99cc40576f52d55c888fbf1001ed4d",
+    "url": "/static/media/xd-grasp.4a99cc40.png"
+  },
+  {
+    "revision": "203cd627ca337dba422d30d5b289dfad",
+    "url": "/static/media/tron.203cd627.png"
+  },
+  {
+    "revision": "f152e94d43939813cb47eebb4edb2645",
+    "url": "/static/media/matchups.f152e94d.png"
+  },
+  {
+    "revision": "4d2aac39e4a64258d7cef3c78d1e36ad",
+    "url": "/static/media/background.4d2aac39.png"
+  },
+  {
+    "revision": "2da26ba88bbc400926c7365bf9f52664",
+    "url": "/static/media/absa.2da26ba8.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4785c2d4ff7ce55cb87c",
+    "url": "/static/js/main.4785c2d4.chunk.js"
+  },
+  {
+    "revision": "657def76d0d59d66c645",
+    "url": "/static/js/1.657def76.chunk.js"
+  },
+  {
+    "revision": "4785c2d4ff7ce55cb87c",
+    "url": "/static/css/main.77826802.chunk.css"
+  },
+  {
+    "revision": "9cd7d5db9e5bf32046fa1ca88440d5be",
+    "url": "/index.html"
+  }
+];
