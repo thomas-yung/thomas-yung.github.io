b2d90eac7405dffbff390a008a1ca3f6820be5b3
Thomas Yung <yung.thomas@outlook.com>
2020-09-10 18:31:22 +0100

    Updates

---
+++ precache-manifest.7b71d5d065a6c1ab250c1301c196f5e8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb152d3647aa12ecac85",
+    "url": "/static/css/main.4f1da140.chunk.css"
+  },
+  {
+    "revision": "bb152d3647aa12ecac85",
+    "url": "/static/js/main.bb152d36.chunk.js"
+  },
+  {
+    "revision": "470effd5af3385980e50",
+    "url": "/static/js/1.470effd5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2da26ba88bbc400926c7365bf9f52664",
+    "url": "/static/media/absa.2da26ba8.png"
+  },
+  {
+    "revision": "f152e94d43939813cb47eebb4edb2645",
+    "url": "/static/media/matchups.f152e94d.png"
+  },
+  {
+    "revision": "203cd627ca337dba422d30d5b289dfad",
+    "url": "/static/media/tron.203cd627.png"
+  },
+  {
+    "revision": "4a99cc40576f52d55c888fbf1001ed4d",
+    "url": "/static/media/xd-grasp.4a99cc40.png"
+  },
+  {
+    "revision": "4d2aac39e4a64258d7cef3c78d1e36ad",
+    "url": "/static/media/background.4d2aac39.png"
+  },
+  {
+    "revision": "154d368329e0a32f5a6d4bb0732f3db4",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.511bb6548a6d4f3682cbc775f999ad00.js"
+  "/precache-manifest.7b71d5d065a6c1ab250c1301c196f5e8.js"
 );
 
 workbox.clientsClaim();
@@ -30,5 +30,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute("/index.html", {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
